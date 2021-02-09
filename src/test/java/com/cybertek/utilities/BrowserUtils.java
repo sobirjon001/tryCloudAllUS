fa5f0e3d24bfecbc25b08a57e8a24084f62634e9
sobirjon001 <sobirjon001@gmail.com>
2021-02-08 19:58:44 -0500

    Sobir created BrowserUtils interface

---
+++ src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -0,0 +1,14 @@
+package com.cybertek.utilities;
+
+public interface BrowserUtils {
+
+  static void sleep(int seconds){
+    seconds *= 1000;
+    try {
+      Thread.sleep(seconds);
+    } catch (InterruptedException e) {
+      System.out.println("Something happened in sleep method!");
+    }
+  }
+
+}
